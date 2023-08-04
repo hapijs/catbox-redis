727f88dff585f8823f461aedc52d63e5e3805550
Nicolas Morel <nicolas@morel.io>
2023-08-04 16:07:36 +0200

    chore: enable lab type tests (#128)
    Follow-up to #126.

--- test/types.ts
+++ test/types.ts
@@ -1,24 +1,39 @@
 import { Engine } from '..';
 import Redis, { Cluster } from 'ioredis';
 
-const cache = new Engine<string>({
-    client: new Redis(),
-    host: 'localhost',
-    partition: 'test',
-    port: 2018,
-});
+async function test() {
+    const cache = new Engine<string>({
+        client: new Redis(),
+        host: 'localhost',
+        partition: 'test',
+        port: 2018,
+    });
 
-cache.get({
-    segment: 'test',
-    id: 'test',
-});
+    await cache.start()
 
-cache.set({
-    segment: 'test',
-    id: 'test',
-}, 'test', 123);
+    cache.get({
+        segment: 'test',
+        id: 'test',
+    });
+
+    cache.set({
+        segment: 'test',
+        id: 'test',
+    }, 'test', 123);
 
 
-new Engine({
-    client: new Cluster([new Redis(), new Redis()])
-});
+    new Engine({
+        client: new Cluster([
+            {
+                host: '127.0.0.1',
+                port: 27379
+            },
+            {
+                host: '127.0.0.2',
+                port: 27379
+            }
+        ])
+    });
+}
+
+test();
